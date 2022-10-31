d1f78afaedf5d9e4e983198625270ce275903ed9
West <76145386+westmcmillen@users.noreply.github.com>
2022-10-30 21:37:32 -1000

    Update navbar.component.ts

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { faArchive, faPenToSquare } from '@fortawesome/pro-duotone-svg-icons';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,4 +10,15 @@ import { faArchive, faPenToSquare } from '@fortawesome/pro-duotone-svg-icons';
 export class NavbarComponent {
   faArchive = faArchive;
   faPenToSquare = faPenToSquare;
+
+  href: string = '';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.href = event.url;
+        console.log(this.href);
+      }
+    });
+  }
 }
