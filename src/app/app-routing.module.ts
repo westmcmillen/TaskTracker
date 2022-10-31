7d3bb423d3c426f220b3132cfadefe02cd8d2e37
West <76145386+westmcmillen@users.noreply.github.com>
2022-10-30 19:51:08 -1000

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArchivesComponent } from './components/archives/archives.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tasks', component: TasksComponent },
+  { path: 'archives', component: ArchivesComponent },
+  { path: '**', redirectTo: '/tasks' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
