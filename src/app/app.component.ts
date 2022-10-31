1e0f2d9b3f93524bc5d1c4056933d94e3ddbe086
West <76145386+westmcmillen@users.noreply.github.com>
2022-10-30 19:51:02 -1000

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { TaskService } from './services/task.service';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './services/data.service';
+import { Task } from 'interface';
+import { ArchiveService } from './services/archive.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'task-tracker';
+export class AppComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(
+    private taskService: TaskService,
+    private archiveService: ArchiveService,
+    private dataService: DataService,
+  ) {}
+
+  ngOnInit(): void {
+    this.taskService.fetch().subscribe((tasks: Task[]) => {
+      this.dataService.setTasks(tasks);
+    });
+
+    this.archiveService.fetch().subscribe((tasks: Task[]) => {
+      this.dataService.setArchives(tasks);
+    });
+
+    this.dataService.currentTasks.subscribe((tasks: Task[]) => {
+      this.tasks = tasks;
+    });
+
+    this.dataService.currentArchives.subscribe((tasks: Task[]) => {
+      this.tasks = tasks;
+    });
+  }
 }
